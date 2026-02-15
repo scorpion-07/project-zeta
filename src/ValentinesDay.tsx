670e7601f4920d99f08016155cbd341a9536e51b
adhirajnandy <adhirajnandy9@gmail.com>
2026-02-15 22:55:23 +0530

    UPD(

--- src/ValentinesDay.tsx
+++ src/ValentinesDay.tsx
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-
-export default function ValentinesDay() {
-=======
 import React, { useState } from 'react'
 
 export default function ValentinesDay() {
@@ -19,7 +14,6 @@ export default function ValentinesDay() {
       "You make spaces feel softer. Conversations feel easier. Days feel lighter."
   }
 
->>>>>>> a19871328d17249d9a88c44956db74a21d150f82
   return (
     <section
       style={{
@@ -47,63 +41,6 @@ export default function ValentinesDay() {
 
       <h2
         style={{
-<<<<<<< HEAD
-          fontSize: 30,
-          fontWeight: 700,
-          color: '#9d174d',
-          marginBottom: 20,
-        }}
-      >
-        Happy Valentine’s Day 🤍
-      </h2>
-
-      <p
-        style={{
-          fontSize: 16,
-          color: '#374151',
-          maxWidth: 340,
-          lineHeight: 1.8,
-          marginBottom: 24,
-        }}
-      >
-        I didn’t make this week to impress you.
-        <br /><br />
-        I made it because you matter to me.
-        <br /><br />
-        Because I admire who you are.
-        Because I respect you.
-        Because being with you feels steady and real.
-        <br /><br />
-        And that’s not something I take lightly.
-      </p>
-
-      <p
-        style={{
-          fontSize: 16,
-          color: '#374151',
-          maxWidth: 340,
-          lineHeight: 1.8,
-        }}
-      >
-        I don’t promise perfection.
-        I don’t promise dramatic gestures every day.
-        <br /><br />
-        But I do promise intention.
-        Effort.
-        And choosing you, even on ordinary days.
-      </p>
-
-      <div
-        style={{
-          marginTop: 30,
-          fontSize: 18,
-          fontWeight: 600,
-          color: '#7e22ce',
-        }}
-      >
-        I’m really glad it’s you. 💞
-      </div>
-=======
           fontSize: 28,
           fontWeight: 600,
           color: '#9d174d',
@@ -176,7 +113,6 @@ export default function ValentinesDay() {
         <br /><br />
         I’m really glad it’s you. 🤍
       </p>
->>>>>>> a19871328d17249d9a88c44956db74a21d150f82
     </section>
   )
 }
